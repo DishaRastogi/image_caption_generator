934ca2b00fd84e19617b47936a07e8d2c64b0600
Disha Rastogi <106084240+DishaRastogi@users.noreply.github.com>
2025-06-25 09:12:43 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import styles from '../styles.module.css';
-import projLogoVideo from '../videos/proj_logo.mp4';
-import projectLogoVideo from '../videos/project_logo.mp4';
-import catImage from '../background/cat.png';
+// import Logo from '../background/logo.png';
+import HomePageBg from '../background/HomePageBg.jpg';
 import { Link } from 'react-router-dom';
 
 
 const Home = () => {
-  
+
   return (
     <div className={styles.topContainer}>
-      <video id="bg-video" autoPlay loop muted>
-        <source src={projectLogoVideo} type="video/mp4" />
-      </video>
-      <div className={styles.login}>
-        <Link className={styles.logbtn} to="/login">
-          Login
-        </Link>
-      </div>
+      <img className={styles.Home} src={HomePageBg} alt="HomeBg" />
+
+      {/* <img className={styles.logo} src={Logo} alt="logo" /> */}
+
       <div className={styles.info}>
-        <h2>The Next-Gen Image Insight Generation</h2>
-        <div className={styles.description}>
-        <img className={styles.cat} src={catImage} alt="Cat" />
-          <h3>
-            Let our AI do the talking! Generate captions for your images in
-            seconds....
-          </h3>
-          <h3>So come, upload your images and listen to them come to life</h3>
-        </div>
+
+        <h2>Image Caption Generator</h2>
+
         <div className={styles.getStarted}>
           <Link className={styles.btn} to="/upload">
             Get Started!
           </Link>
+
         </div>
       </div>
     </div>
